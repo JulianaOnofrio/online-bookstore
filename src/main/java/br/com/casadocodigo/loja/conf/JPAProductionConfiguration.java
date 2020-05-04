7d858e4cf5103afe0eb3028607bfbe09abe3ca6e
JulianaOnofrio <57050340+JulianaOnofrio@users.noreply.github.com>
2020-05-04 18:28:04 -0300

    Update JPAProductionConfiguration.java

--- src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
@@ -10,12 +10,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-/** Concentrando as configurações do JPA de produção */
-
 @Profile("prod")
 public class JPAProductionConfiguration {
 
-    //necessário para carregar as variáveis de ambiente
     @Autowired
     private Environment environment;
 
@@ -31,17 +28,9 @@ public class JPAProductionConfiguration {
 	@Bean	
 	private DriverManagerDataSource dataSource() throws URISyntaxException {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        //alterado driver p postgresql
         dataSource.setDriverClassName("org.postgresql.Driver");
-
-        //variavel de ambiente do Heroku para obter USR/PWD do BD
-        //devolve -> user:password@host:port/path
         URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
-        
-        //setando as configurações do DB com base nos dados fornecidos pelo Heroku
-        //através da variável de ambiente "DATABASE_URL" > dbUrl
         dataSource.setUrl("jdbc:postgresql://"+dbUrl.getHost()+":"+dbUrl.getPort()+dbUrl.getPath());
-        //utilizado split > info do usr vem em uma mesma String com ':' como separador
         dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
         dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
     
