91510f1c97a6a28e39930a1b3319bcc1beef5d76
JulianaOnofrio <57050340+JulianaOnofrio@users.noreply.github.com>
2020-05-04 18:21:22 -0300

    Update AppWebConfiguration.java

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -42,37 +42,21 @@ import br.com.casadocodigo.loja.dao.ProdutoDAO;
 import br.com.casadocodigo.loja.infra.FileSaver;
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 
-/**
- * Classe base da Aplicação
- * 
- * Faz o direcionamento inicial do fluxo da aplicação
- * */
 
-
-//habilita o Spring WEB
 @EnableWebMvc
-//informa as classes (e seus pacotes) onde estarão os controllers de acesso - Aplicação e Banco
-//todos os pacotes úteis para a aplicação devem ser informados 
 @ComponentScan(basePackageClasses= {HomeController.class, ProdutoDAO.class, 
 		FileSaver.class, CarrinhoCompras.class})
-
-//habilita o cache		
-@EnableCaching //>>>comentar essa notação para testes no JUnit
-//WebMvcConfigurerAdapter - auxilia no mapeamento dos arquivos js e css
+		
+@EnableCaching 
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	
-	//prefixo "/WEB-INF/views/" e sufixo  ".jsp"
-	@Bean //classe spring q auxilia no mapeamento das paginas
+	
+	@Bean 
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix( ".jsp");
 		
-		//Spring "disponibiliza" todos os Beans para os JSPs > JSP conseguem acessar as classes 'Bean'
-		//resolver.setExposeContextBeansAsAttributes(true);
-		
-		//Disponibilizando pontualmente os Beans > melhor em termos de segurança
-		//Informar por String o nome da Classe com a primeira letra 'minúscula'
 		resolver.setExposedContextBeanNames("carrinhoCompras");
 		
 		
@@ -80,23 +64,19 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	}
 	
 	@Bean
-	//configurandoo arquivo padrão de mensagens >> message.proprieties
 	public MessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource 
 		= new ReloadableResourceBundleMessageSource();
 		
-		//apontando o caminho para a o arquivo com as mensagens de erro > arquivo - 'messages'.*
 		messageSource.setBasename("/WEB-INF/messages");
-		//setando o padrão de encoding
+		
 		messageSource.setDefaultEncoding("UTF-8");
-		//o tempo q o arquivo atualiza quando uma alteração é feita
+		
 		messageSource.setCacheSeconds(1);
 		
 		return messageSource;
 	}
 	
-	//definindo as configurações de data
-	//notações ao longo do codigo não serão mais necessárias
 	@Bean
 	public FormattingConversionService mvcConversionService() {
 		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
@@ -108,26 +88,17 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	}
 	
 	@Bean
-	//crianto metodo para tratar o multpart
 	public MultipartResolver multipartResolver() {
 		return new StandardServletMultipartResolver();
 	}
-	
-	//anotando o RestTemplate para Spring Reconhece-lo
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-
-	//metodo que cria o cache - CacheManager não pode ser usado um produção
-	//cache em produção utilizar o 'Guava' do Google
+	
 	@Bean
 	public CacheManager cacheManager(){
-		/** configurando o Cache
-		 * - ativo por 5 minutos
-		 * - guarda até 100 elementos
-		 * Obs.: config básica p testes em desenvolvimento
-		 */
+		
 		CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder()
 			.maximumSize(100)
 			.expireAfterAccess(5, TimeUnit.MINUTES);
@@ -137,15 +108,13 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 		return manager;
 	}
 
-	//configurando a Resposta do sistema. Cofnigurar se o mesmo vai responder
-	//como HTML ou JSON
 	@Bean
 	public ViewResolver contentNegociationViewResolver(ContentNegotiationManager manager){
-		//criando uma lista de ViewResolver - HTML, JSON
+		
 		List<ViewResolver> viewResolvers = new ArrayList<>();
-		//resolve o HTML
+		
 		viewResolvers.add(internalResourceViewResolver());
-		//resolve o JSON
+		
 		viewResolvers.add(new JsonViewResolver());
 
 		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
@@ -155,7 +124,6 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 		return resolver;
 	}
 
-	//aponta qual servlet vai mapear as requisições default para arquivos js/css
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
     configurer.enable();
