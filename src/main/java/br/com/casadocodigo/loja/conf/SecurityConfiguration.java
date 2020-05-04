0a2c00ec7bf6196f390472b08a19368bc77057d4
JulianaOnofrio <57050340+JulianaOnofrio@users.noreply.github.com>
2020-05-04 18:31:00 -0300

    Update SecurityConfiguration.java

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -11,27 +11,15 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.com.casadocodigo.loja.dao.UsuarioDAO;
 
-
-
-/** Classe com as configurações de segurança
- * 
- * habilita filtro de login
- * 
- */
-
-
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     
     @Autowired
     private UsuarioDAO usuarioDAO;
 
-    //metodo de configuração
-    //configura quem pode acesar o quê
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-        //** - relaciona todos os itens após a '/'' /
         .antMatchers("/produtos/form").hasRole("ADMIN")
         .antMatchers("/carrinho/**").permitAll()
         .antMatchers("/pagamento/**").permitAll()
@@ -39,20 +27,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
         .antMatchers("/produtos/**").permitAll()
         .antMatchers("/resources/**").permitAll()
-        //após config as restrições o que não foi mencionado será liberado
         .antMatchers("/").permitAll()
         .antMatchers("/url-magica-maluca-oajksfbvad6584i57j54f9684nvi658efnoewfmnvowefnoeijn").permitAll()
         .anyRequest().authenticated()
-        //setando a pagina de login loginForm.jsp e permitindo a todos
         .and().formLogin().loginPage("/login")
             .defaultSuccessUrl("/produtos").permitAll()
         .and().logout()
             .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .permitAll().logoutSuccessUrl("/login");    
     }
-
-    /** Necessário implementar classes Usuario, UsuarioDAO e Role e implementar
-     * metodos para implementar o controle de usuarios no sistema*/
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(usuarioDAO)
