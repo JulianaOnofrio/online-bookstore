074c3a6aa40d306bab65612e93eba9bf78518eca
JulianaOnofrio <57050340+JulianaOnofrio@users.noreply.github.com>
2020-05-04 18:30:10 -0300

    Update JsonViewResolver.java

--- src/main/java/br/com/casadocodigo/loja/conf/JsonViewResolver.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JsonViewResolver.java
@@ -9,10 +9,8 @@ import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 public class JsonViewResolver implements ViewResolver{
 
     @Override
-    //tratando o retorno da resposata em forma de JSON
 	public View resolveViewName(String viewName, Locale locale) throws Exception {
         MappingJackson2JsonView jsonView = new MappingJackson2JsonView();
-        //habilitando print personalisado do JSON
         jsonView.setPrettyPrint(true);
 
         return jsonView;
