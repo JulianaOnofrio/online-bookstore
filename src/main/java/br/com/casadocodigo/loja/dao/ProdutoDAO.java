4937e08104546043eea22f021ecbfdd342ded050
JulianaOnofrio <57050340+JulianaOnofrio@users.noreply.github.com>
2020-05-04 18:41:36 -0300

    Update ProdutoDAO.java

--- src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
+++ src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
@@ -13,17 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 
-/**Classeo DAO são responsáveis pelo acesso às entidades do banco
- * 
- * ProdutoDAO trata apenas da entidade Produto
- * */
-
-
 @Repository
 @Transactional
 public class ProdutoDAO {
 
-	//persistindo o 'EntityManager' criado junto ao spring
 	@PersistenceContext
 	private EntityManager manager;
 	
@@ -31,16 +24,13 @@ public class ProdutoDAO {
 		manager.persist(produto);
 	}
 
-	//trabalhando com tipos diferentes de preço - formando ujma lista
 	public List<Produto> listar() {
-		//getResultList() retorna o resultado da busca createQuery em forma de List
 		return manager.createQuery("select distinct(p) from Produto p join fetch p.precos", Produto.class)
 				.getResultList();
 	}
 
-	//Além do produto, trazendo tb os precos > 'fecth join'
 	public Produto find(Integer id) {
-		//buscando no banco o produto pelo 'id'
+
 		return manager.createQuery("select distinct(p) from Produto p "
 				+ "join fetch p.precos preco where p.id = :id", Produto.class)
 				.setParameter("id", id)
@@ -48,7 +38,6 @@ public class ProdutoDAO {
 	}
 
 
-	//Relatório de vendas
 	public BigDecimal somaPrecosPorTipoPreco(TipoPreco tipoPreco){
 		TypedQuery<BigDecimal> query =  manager
 			.createQuery("select sum(preco.valor) from Produto p "
