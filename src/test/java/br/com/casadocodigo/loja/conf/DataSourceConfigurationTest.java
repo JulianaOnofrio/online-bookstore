6eeac33177365f80e16ff6cceb821330d77dd28a
JulianaOnofrio <57050340+JulianaOnofrio@users.noreply.github.com>
2020-05-04 18:27:25 -0300

    Update DataSourceConfigurationTest.java

--- src/test/java/br/com/casadocodigo/loja/conf/DataSourceConfigurationTest.java
+++ src/test/java/br/com/casadocodigo/loja/conf/DataSourceConfigurationTest.java
@@ -6,19 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-/**Para evitar alterações na classe JPA principal
- * 
- * Esta classe foi configurada para acessar o BD de teste
- * 
- * e fazer os teste sem afetar o cod principal
- * 
- * */
-
 
 public class DataSourceConfigurationTest {
 	
-	//setando o profile como test e o spring faz a ponte 
-	//entre as classers setadas como 'test'
 	@Bean
 	@Profile("test")
 	public DataSource dataSource() {
@@ -26,7 +16,7 @@ public class DataSourceConfigurationTest {
 		dataSource.setUrl("jdbc:mysql://localhost/casadocodigo_test");
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUsername("root");
-		dataSource.setPassword("#Diego1983#");
+		dataSource.setPassword("");
 		
 		return dataSource;
 	}
